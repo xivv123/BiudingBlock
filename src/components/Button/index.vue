466af423e778d7a31fa88977b94c3839ee37614b
xivv123 <39082952+xivv123@users.noreply.github.com>
2023-08-24 23:59:28 +0800

    Add files via upload

---
+++ src/components/Button/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <button :class="['button', type]" @click="handleClick">
+    <slot></slot>
+  </button>
+</template>
+
+<script lang="ts">
+import { ref, toRefs } from 'vue';
+
+export default {
+  name: 'B-Button',
+  props: {
+    type: {
+      type: String,
+      default: 'default'
+    }
+  },
+  setup(props, { emit }) {
+    const { type } = toRefs(props);
+
+    const handleClick = (event: Event) => {
+      emit('click', event);
+    };
+
+    return {
+      type,
+      handleClick
+    };
+  }
+};
+</script>
+
+<style scoped>
+.button {
+  padding: 10px 20px;
+  font-size: 16px;
+  border-radius: 5px;
+  border: none;
+  cursor: pointer;
+  transition: background-color 0.3s;
+}
+.button.default {
+  background-color: #3498db;
+  color: white;
+}
+.button.primary {
+  background-color: #2ecc71;
+  color: white;
+}
+.button.danger {
+  background-color: #e74c3c;
+  color: white;
+}
+</style>
---
+++ src/components/Input/index.vue
@@ -0,0 +1,31 @@
+<template>
+    <input :value="modelValue" @input="updateValue" :placeholder="placeholder" />
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent, PropType } from 'vue';
+  
+  export default defineComponent({
+    name: 'B-Input',
+    props: {
+      modelValue: {
+        type: String as PropType<string>,
+        default: ''
+      },
+      placeholder: {
+        type: String as PropType<string>,
+        default: ''
+      }
+    },
+    emits: ['update:modelValue'],
+    setup(props, { emit }) {
+      const updateValue = (event: Event) => {
+        emit('update:modelValue', (event.target as HTMLInputElement).value);
+      };
+  
+      return {
+        updateValue
+      };
+    }
+  });
+  </script>
